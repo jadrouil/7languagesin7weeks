049b27912cd153e2fa0d051f8d8b18fa047086c0
Jerm <jadrouil@umich.edu>
2016-07-18 21:13:34 -0500

    Somehow managed to introduce a race conditon in erlang.

--- Week5-Erlang/day3test.erl
+++ Week5-Erlang/day3test.erl
@@ -2,14 +2,27 @@
 -export([all/0]).
 -import(day3,[doctor/0, ask_translation/1]).
 
+setup() -> 
+        Dr = spawn(fun day3:doctor/0),
+        Dr ! new,
+        timer:sleep(1000), 
+        Dr.
+
+ %The registered processes are shared across all processes.
+ %So, even though erlang is functional, there is still room for race conditions.
+ %When the doctor process is spawned, it won't necessarily finish constructing the translator
+ %before the following test begins to run.
+ %This causes the test to run without translator being registered as a process.
+ %This will cause an error.
+ 
+ %This is also why I was able to run the test in the command line without an issue. 
+ %I'm too slow to ever win the race with doctor.
+
 all()->
-    Dr = spawn(fun day3:doctor/0),
-    Dr ! new,
-    ShouldBeHome = "home",
-    registered(),
-    ShouldBeHome = day3:ask_translation("casa").
-%For whatever reason running the above steps from the command line isn't an issue.
-%However, here ask_translation fails with bad argument. I think the reason for this is
-% the translator atomic isn't recognized across files.
-% the call to registered() shows that translator is not registered.
-%will fix tomorrow.
+    setup(),
+    ShouldBeHouse = "house",
+    Translate = "casa",
+    ShouldBeHouse = day3:ask_translation(Translate).
+    
+
+

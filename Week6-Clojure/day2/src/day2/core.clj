a2b0c75c4c5faa432d9e3bdf8f4228168b7c6433
Jerm <jadrouil@umich.edu>
2016-07-26 18:38:20 -0500

    done with day 2 of clojure

---
+++ Week6-Clojure/day2/src/day2/core.clj
@@ -0,0 +1,29 @@
+(ns day2.core)
+
+(defmacro unless [test body else]
+    (list 'if (list 'not test) body else))
+
+(defprotocol VendingMachine
+    (dispense [c])
+    (restock [c]))
+
+(defrecord CokeVendingMachine [originalStock]
+    VendingMachine
+    (dispense [c] (CokeVendingMachine. (- originalStock 1)))
+    (restock [c] (CokeVendingMachine. (+ originalStock 1))))
+  
+(defn -main []
+    (assert (= 1 (unless false 1 2)))
+    (assert (= 2 (unless true 1 2)))
+    (println "done with macro\n")
+    (def undertest (CokeVendingMachine. 3))
+    (assert (= (:originalStock undertest) 3))
+    (assert (= (:originalStock (dispense undertest)) 2))
+    (assert (= (:originalStock (restock undertest)) 4))
+    (println "done with VendingMachine protocol")
+    ;(assert (= (collection-type []) :vector))
+    ;(assert (= (collection-type '(1)) :list))
+    ;(assert (= (collection-type {}) :map))
+    ;(assert (= (collection-type '()) :list))
+    ;(println "done with col type")
+)
